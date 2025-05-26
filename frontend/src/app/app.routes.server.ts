ea914adf8016c451ce64ef83aa1e59a3f109369e
johannes winkler <johannes.w@gmx.at>
2025-05-26 19:19:50 +0000

    Update app.routes.server.ts

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,11 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
+    path: 'recovery/:token',
+    renderMode: RenderMode.Server
   },
   {
-    path: 'recovery/:token',
-    renderMode: RenderMode.Server,
+    path: '**',
+    renderMode: RenderMode.Prerender,
   }
 ];
