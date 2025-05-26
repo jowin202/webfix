bd54b341c6fe02f6cbdbab648414bee53725b3af
Johannes Winkler <johannes.w@gmx.at>
2025-05-26 11:20:05 +0200

    register works (verification TODO)

--- app/routes/register.py
+++ app/routes/register.py
@@ -16,8 +16,21 @@ router = APIRouter()
 manager = SettingsManager()
 
 
+
+class RegisterUserRequest(BaseModel):
+    username: str
+    name: str
+    tel: str
+    mail: str
+    fediverse_id: str
+    password: str
+    verify_mail: bool
+    verify_fediverse: bool
+
+
+
 @router.post("/register/")
-async def register_user(username : str, name : str, tel : str, mail : str, fediverse_id : str, password : str, verify_mail : bool, verify_fediverse : bool):
+async def register_user(data: RegisterUserRequest):
     conn = await get_pg_connection() 
     verification = manager.get_setting("mandatory_user_verification")
     try:
@@ -27,27 +40,27 @@ async def register_user(username : str, name : str, tel : str, mail : str, fediv
             await conn.execute('''
                 INSERT INTO users (username, username_html, name, tel, mail, fediverse_id, password, token, is_activated, activation_token) 
                 VALUES ($1,'<b>' || $1::varchar || '</b>',$2,$3,$4,$5,$6,'', false, $7)
-            ''', username, name, tel, mail, fediverse_id, calc_hmac(password), activation_token
+            ''', data.username, data.name, data.tel, data.mail, data.fediverse_id, calc_hmac(data.password), activation_token
             )
 
-            if verify_mail:
-                mail_body = "Hello " + name + "\n\n"
+            if data.verify_mail:
+                mail_body = "Hello " + data.name + "\n\n"
                 mail_body += "Welcome to the chat.\n"
                 mail_body += "To activate your account, please click the link below or paste it into your browser:\n\n"
                 mail_body += os.getenv('PROTOCOL') + "://" + os.getenv("DOMAIN_NAME") + "/activate/" + activation_token + "/\n\n"
                 mail_body += "Kind regards"
-                send_mail(mail, "Activate your account", mail_body)
+                send_mail(data.mail, "Activate your account", mail_body)
 
-            if verify_fediverse:
+            if data.verify_fediverse:
                 text = "Hi, Activation Link: "
                 text += os.getenv("DOMAIN_NAME") + "/activate/" + activation_token + "/"
-                send_fediverse(fediverse_id, text)
+                send_fediverse(data.fediverse_id, text)
 
         else:
             await conn.execute('''
                 INSERT INTO users (username, username_html, name, tel, mail, fediverse_id, password, token) 
                 VALUES ($1,'<b>' || $1::varchar || '</b>',$2,$3,$4,$5,$6,'')
-            ''', username, name, tel, mail, fediverse_id, calc_hmac(password)
+            ''', data.username, data.name, data.tel, data.mail, data.fediverse_id, calc_hmac(data.password)
             )
 
 
@@ -55,7 +68,7 @@ async def register_user(username : str, name : str, tel : str, mail : str, fediv
         raise HTTPException(status_code=400, detail=str(e))
     finally:
         await release_pg_connection(conn)
-    return True
+    return {"succecss": True }
 
 
 # also check admin activate account method in admin/admin
--- frontend/src/app/components/register-window/register-window.component.ts
+++ frontend/src/app/components/register-window/register-window.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-register-window',
@@ -11,4 +12,34 @@ import { RouterModule } from '@angular/router';
 })
 export class RegisterWindowComponent {
 
+  constructor (private api : ApiService){}
+
+  error : Boolean = false;
+  success: Boolean = false;
+
+  do_register(content : any)
+  {
+    this.success = false;
+      this.api.post("/api/register/register/", "", {
+              "username": content.username,
+              "name": content.name,
+              "tel": content.tel,
+              "mail": content.email,
+              "fediverse_id": content.fediverse,
+              "password": content.password,
+              "verify_mail": false,
+              "verify_fediverse": false
+            })
+      .subscribe(result => {
+        if ("error_code" in result)
+          this.error = true;
+        else 
+        {
+          this.error = false;
+          this.success = true;
+        }
+      });
+    
+
+  }
 }
