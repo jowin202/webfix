5b6cb0d0e81ca88cc4dee2ac171cdfa5bbd8a3c3
Johannes Winkler <johannes.w@gmx.at>
2025-05-27 08:46:27 +0200

    restore

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,12 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  /*{
+  {
     path: 'recovery/:token',
     renderMode: RenderMode.Client
-  },*/
+  },
   {
     path: '**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Prerender,
   }
 ];
