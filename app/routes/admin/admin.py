fbb26b921a709491774dfbcc147598cca7b7bbc7
Johannes Winkler <johannes.w@gmx.at>
2025-06-04 15:14:10 +0200

    mute should work, kick prepared

--- app/routes/admin/admin.py
+++ app/routes/admin/admin.py
@@ -44,3 +44,99 @@ async def reset_database():
     await pg_db_init()
     return True
 
+
+
+@router.post("/kick_user/{username}/{time}/")
+async def kick_user(username : str, time : int):
+    conn = await get_pg_connection() 
+    status = True
+
+    try:
+        if time > 0:
+            query = '''
+                UPDATE users
+                SET kicked_until = NOW() + $2
+                WHERE username = $1;
+                '''
+            await conn.execute(query, username, time)
+        elif time == 0:
+            query = '''
+                UPDATE users
+                SET kicked_until = 'infinity'
+                WHERE username = $1;
+                '''
+            await conn.execute(query, username)
+    except:
+        status = False
+    finally:
+        await release_pg_connection(conn)
+    return status
+
+
+@router.post("/mute_user/{username}/{time}/")
+async def mute_user(username : str, time : int):
+    conn = await get_pg_connection() 
+    status = True
+
+    try:
+        if time > 0:
+            query = '''
+                UPDATE users
+                SET muted_until = NOW() + (INTERVAL '1 second' * $2)
+                WHERE username = $1;
+                '''
+            await conn.execute(query, username, time)
+        elif time == 0:
+            query = '''
+                UPDATE users
+                SET muted_until = 'infinity'
+                WHERE username = $1;
+                '''
+            await conn.execute(query, username)
+    except Exception as e:
+        status = False
+    finally:
+        await release_pg_connection(conn)
+    return status
+
+
+
+
+
+@router.post("/unkick_user/{username}/")
+async def unkick_user(username : str):
+    conn = await get_pg_connection() 
+    status = True
+
+    try:
+        query = '''
+            UPDATE users
+            SET kicked_until = '-infinity'
+            WHERE username = $1;
+            '''
+        await conn.execute(query, username)
+    except:
+        status = False
+    finally:
+        await release_pg_connection(conn)
+    return status
+
+
+@router.post("/unmute_user/{username}/")
+async def unmute_user(username : str):
+    conn = await get_pg_connection() 
+    status = True
+
+    try:
+        query = '''
+            UPDATE users
+            SET muted_until = '-infinity'
+            WHERE username = $1;
+            '''
+        await conn.execute(query, username)
+    except:
+        status = False
+    finally:
+        await release_pg_connection(conn)
+    return status
+
--- app/routes/input.py
+++ app/routes/input.py
@@ -19,27 +19,46 @@ class SendMessageRequest(BaseModel):
 async def write_text(data: SendMessageRequest, request: Request):
     conn = await get_pg_connection()
     query = """
-        SELECT username, channel_id, last_posted
+        SELECT username, channel_id, last_posted,id,
+        CASE 
+            WHEN muted_until = 'infinity'::timestamp 
+                THEN 9223372036854775807
+            WHEN muted_until = '-infinity'::timestamp 
+                OR muted_until - NOW() < INTERVAL '0 seconds'
+                THEN 0
+            ELSE EXTRACT(EPOCH FROM (muted_until - NOW()))
+        END AS muted_seconds
         FROM users 
         WHERE id = $1
     """
     result = await conn.fetchrow(query, request.state.user_id)
 
-    query = """
-        UPDATE users
-        SET online_time = online_time + EXTRACT(EPOCH FROM (NOW() - last_posted))::int,
-        last_posted = NOW()
-        WHERE id = $1
-    """
-    await conn.execute(query, request.state.user_id)
 
-    channel_id = int(result['channel_id']) if result else -1
-    payload = json.dumps({
-    "username": result['username'],
-    "message": data.message
-    })
-    quoted_payload = await conn.fetchval("SELECT quote_literal($1)", payload)
-    await conn.execute(f"NOTIFY channel_{channel_id}, {quoted_payload}")
+    if result:
+        # timeout prevention
+        query = """
+            UPDATE users
+            SET online_time = online_time + EXTRACT(EPOCH FROM (NOW() - last_posted))::int,
+            last_posted = NOW()
+            WHERE id = $1
+        """
+        await conn.execute(query, request.state.user_id)
+
+        id = result['id']
+        channel_id = int(result['channel_id']) if result else -1
+        muted_seconds = result['muted_seconds']
+        
+        if muted_seconds <= 0:
+            payload = json.dumps({
+            "username": result['username'],
+            "message": data.message
+            })
+            quoted_payload = await conn.fetchval("SELECT quote_literal($1)", payload)
+            await conn.execute(f"NOTIFY channel_{channel_id}, {quoted_payload}")
+        else:
+            await conn.execute(f"NOTIFY channel_{channel_id}, '{json.dumps({'cat': 'statusmsg', 'username': '', 'msg': 'You are muted for ' + str(int(muted_seconds)) + ' seconds.'})}'")
+
+            await conn.execute(f"NOTIFY whisper_{id}, 'You are muted for {muted_seconds} more seconds.'")
 
     await release_pg_connection(conn)
     return {"status": "notification sent", "message": data.message}
