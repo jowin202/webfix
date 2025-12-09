59ce1962f587e90fe7d96602ff3c8d4b95d4ff4c
Johannes Winkler <johannes.w@gmx.at>
2025-12-09 06:47:24 +0100

    timeout script in external file

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ from fastapi import WebSocketDisconnect, Depends
 
 from fastapi.templating import Jinja2Templates
 
-from routes.login import logout_token
 
 import os 
 import json
@@ -19,6 +18,7 @@ from security import verify_token, verify_token_admin
 from db import pg_db_init, get_pg_connection, initialize_connection_pool, release_pg_connection
 
 from settings import SettingsManager
+from timeout import timeout_check
 
 
 
@@ -71,36 +71,6 @@ async def lifespan(app: FastAPI):
     yield # this is mandatory
 
 app = FastAPI(title="Webfix API", lifespan=lifespan)
-
-async def timeout_check():
-    refresh_intervall = 10
-    manager = SettingsManager()
-    while True:
-        conn = await get_pg_connection()
-        
-        result = await conn.fetch("""SELECT id, username, token, NOW()-last_posted AS diff FROM users WHERE last_posted IS NOT NULL AND last_posted < NOW() - ($1 || ' seconds')::interval AND token != '' """, str(manager.get_setting("timeout_time")))
-        for row in result:
-            msg = '{"cat": "statusmsg", "msg": "Timeout"}'
-            await conn.execute(f"NOTIFY whisper_{row['id']}, '{msg}' ")
-            await logout_token(row['token']) # from endpoint 
-            
-
-        row = await conn.fetchrow("""
-            SELECT MIN((last_posted + ($1 || ' seconds')::interval) - NOW()) AS remaining
-            FROM users
-            WHERE token != ''
-            AND last_posted IS NOT NULL
-            AND last_posted + ($1 || ' seconds')::interval > NOW()
-                                           """, str(manager.get_setting("timeout_time")))
-        
-        if row and row['remaining']:
-            refresh_intervall = int(row['remaining'].total_seconds())+5 # 5 sec tolerance for testing
-        else:
-            refresh_intervall = int(manager.get_setting("timeout_time")/2)  # or 0 or some fallback
-
-        # print(refresh_intervall, flush=True)
-        await release_pg_connection(conn)
-        await asyncio.sleep(refresh_intervall)
 
 
 
